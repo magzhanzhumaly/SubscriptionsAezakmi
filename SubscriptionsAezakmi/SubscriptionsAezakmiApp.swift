5f1066703ab356809b0fd497bc49e22f823d948e
Magzhan Zhumaly <magzhanzhumaly@gmail.com>
2024-06-17 13:53:12 +0500

    Initial Commit

---
+++ SubscriptionsAezakmi/SubscriptionsAezakmiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubscriptionsAezakmiApp.swift
+//  SubscriptionsAezakmi
+//
+//  Created by Magzhan Zhumaly on 17.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SubscriptionsAezakmiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
